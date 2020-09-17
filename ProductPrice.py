7d40691d157fde5cbd2c2b459a5e1d378018fbe3
Avinash Pulusu <pulusuar@mail.uc.edu>
2020-09-17 14:32:48 -0400

    Add files via upload

---
+++ ProductPrice.py
@@ -0,0 +1,49 @@
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+
+URL = 'https://www.amazon.com/Beats-Wireless-Noise-Cancelling-Headphones-Bluetooth/dp/B07YVZ15SG?ref_=Oct_DLandingS_D_ec41923b_61&smid=ATVPDKIKX0DER'
+headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'}
+
+def check_price():
+    page = requests.get(URL, headers=headers)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    title = soup.find(id="productTitle").get_text().strip()
+
+    dealprice = 0
+    dealprice = soup.find(id="priceblock_dealprice").get_text().strip()
+    if dealprice:
+        converted_price = float(dealprice[1:])
+
+    orig_price = 0
+    if converted_price:
+        orig_price = converted_price
+    originalprice = soup.find("span", {"class": "priceBlockStrikePriceString a-text-strike"}).get_text().strip()
+    if(originalprice):
+        orig_price = float(originalprice[1:])
+
+    if(converted_price < (orig_price * 0.75)):
+        send_mail()
+
+def send_mail():
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    #Give your email and password here
+    server.login(user_name, password)
+
+    subject = 'Price dropped to less than 75%! Wanna buy now?'
+    body = 'Check the Amazon link : https://www.amazon.com/Beats-Wireless-Noise-Cancelling-Headphones-Bluetooth/dp/B07YVZ15SG?ref_=Oct_DLandingS_D_ec41923b_61&smid=ATVPDKIKX0DER'
+    msg = f"Subject: {subject}\n\n{body}"
+
+    server.sendmail(
+        from_email,
+        to_email(s),
+        msg
+    )
+    server.quit()
+
+while(True):
+    check_price()
+    time.sleep(60 * 60 * 24)
